fd7c75322ca7c5f3b66d403f29be8fb656af0f83
Cristiano Badalotti <cristiano.badalotti@gmail.com>
2020-04-08 20:56:23 -0300

    006 - Correções Relatorios

--- src/main/java/com/elecao/teste/java/teste/java/Controles/RelarorioControle.java
+++ src/main/java/com/elecao/teste/java/teste/java/Controles/RelarorioControle.java
@@ -1,15 +1,21 @@
 package com.elecao.teste.java.teste.java.Controles;
 
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.elecao.teste.java.teste.java.Entidades.Candidato;
+import com.elecao.teste.java.teste.java.Entidades.Cargo;
 import com.elecao.teste.java.teste.java.Entidades.Eleicao;
+import com.elecao.teste.java.teste.java.Repository.CandidatoRepository;
+import com.elecao.teste.java.teste.java.Repository.CargoRepository;
 import com.elecao.teste.java.teste.java.Repository.EleicaoRepository;
 import com.elecao.teste.java.teste.java.Util.Convercoes;
 import com.elecao.teste.java.teste.java.report.GeracaoDeRelatorios;
@@ -19,24 +25,62 @@ public class RelarorioControle {
 	@Autowired
 	private EleicaoRepository eleicaoDAO;
 
+	@Autowired
+	private CargoRepository cargoDAO;
+
+	@Autowired
+	private CandidatoRepository candidatoDAO;
+
 	@RequestMapping("/relatorios")
 	public String cadastrarEleicao() {
 		return "relatorio/Relatorios";
 	}
 
 	@GetMapping(value = "/relatorioFinal")
-	public ModelAndView relatorioElecoesFinalizadas() {
+	public ModelAndView relatorioElecoesFinalizadas(RedirectAttributes attributes) {
 		String dataS = new Convercoes().dataAtualUS();
-		System.out.println(dataS);
-		return new ModelAndView(new GeracaoDeRelatorios(), "listaEleicao",
-				eleicaoDAO.findByFimLessThanEqual(dataS));
+
+		List<Eleicao> eleicoes = eleicaoDAO.findByFimLessThanEqual(dataS);
+
+		List<Cargo> cargos = cargoDAO.findAll();
+		List<Candidato> candidatos = (List<Candidato>) candidatoDAO.findAll();
+
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("listaEleicao", eleicoes);
+		model.put("listaCargo", cargos);
+		model.put("listaCandidato", candidatos);
+		String titulo = "Eleições em andamento";
+		model.put("titulo", titulo);
+		String nomeArquivo = "relatorioParcial";
+		model.put("nomeArquivo", nomeArquivo);
+
+		return new ModelAndView(new GeracaoDeRelatorios(), "listaEleicao", model);
+
 	}
 
 	@GetMapping(value = "/relatorioParcial")
-	public ModelAndView relatorioElecoesEmAndamento() {
+	public ModelAndView relatorioElecoesEmAndamento(RedirectAttributes attributes) {
 		String dataS = new Convercoes().dataAtualUS();
-		System.out.println(dataS);
+
 		List<Eleicao> eleicoes = eleicaoDAO.findByFimGreaterThanEqualAndInicioLessThanEqual(dataS, dataS);
-		return new ModelAndView(new GeracaoDeRelatorios(), "listaEleicao", eleicoes);
+		if (eleicoes != null) {
+			List<Cargo> cargos = cargoDAO.findAll();
+
+			List<Candidato> candidatos = (List<Candidato>) candidatoDAO.findAll();
+
+			Map<String, Object> model = new HashMap<String, Object>();
+			model.put("listaEleicao", eleicoes);
+			model.put("listaCargo", cargos);
+			model.put("listaCandidato", candidatos);
+			String titulo = "Eleições em andamento";
+			model.put("titulo", titulo);
+			String nomeArquivo = "relatorioParcial";
+			model.put("nomeArquivo", nomeArquivo);
+
+			return new ModelAndView(new GeracaoDeRelatorios(), model);
+		} else {
+			attributes.addFlashAttribute("mensagem", "Não existe dados de eleições!");
+			return new ModelAndView();
+		}
 	}
 }
--- src/main/java/com/elecao/teste/java/teste/java/report/GeracaoDeRelatorios.java
+++ src/main/java/com/elecao/teste/java/teste/java/report/GeracaoDeRelatorios.java
@@ -6,37 +6,67 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
+import com.elecao.teste.java.teste.java.Entidades.Candidato;
 import com.elecao.teste.java.teste.java.Entidades.Cargo;
 import com.elecao.teste.java.teste.java.Entidades.Eleicao;
-import com.elecao.teste.java.teste.java.Repository.CargoRepository;
+import com.elecao.teste.java.teste.java.Util.Convercoes;
 import com.lowagie.text.Document;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.PdfWriter;
 
+public class GeracaoDeRelatorios extends AbstractPdfView {
 
-
-public class GeracaoDeRelatorios extends AbstractPdfView{
-	
-	@Autowired
-	private CargoRepository cargoDAO;
-	
 	@Override
 	protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer,
 			HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
-		response.setHeader("Content-Disposition", "attachment; filename=\"relatorioEleicoes.pdf\"");
 
 		@SuppressWarnings("unchecked")
-		List<Eleicao> eleicoes = (List<Eleicao>) model.get("listaEleicao");
-		
-		for (Eleicao e: eleicoes) {
-			document.add(new Paragraph(e.getNome() + "   Data de incio: " + e.getInicio() + "    Data de termino:" + e.getFim()));
-			
-			
+		String arquivo = (String) model.get("nomeArquivo");
+		response.setHeader("Content-Disposition", "attachment; filename=\"" + arquivo + ".pdf\"");
+
+		try {
+			@SuppressWarnings("unchecked")
+			List<Eleicao> eleicoes = (List<Eleicao>) model.get("listaEleicao");
+
+			@SuppressWarnings("unchecked")
+			String titulo = (String) model.get("titulo");
+			document.add(new Paragraph(titulo));
+			document.add(new Paragraph(""));
+
+			@SuppressWarnings("unchecked")
+			List<Cargo> cargos = (List<Cargo>) model.get("listaCargo");
+
+			@SuppressWarnings("unchecked")
+			List<Candidato> candidatos = (List<Candidato>) model.get("listaCandidato");
+
+			Convercoes convercoes = new Convercoes();
+
+			for (Eleicao e : eleicoes) {
+				System.out.println(cargos.size());
+				document.add(new Paragraph(
+						e.getNome() + "   Data de incio: " + convercoes.convertDateUStoDataBR(e.getInicio())
+								+ "    Data de termino:" + convercoes.convertDateUStoDataBR(e.getFim())));
+
+				for (Cargo c : cargos) {
+					if (c.getEleicao().getId() == e.getId()) {
+						document.add(new Paragraph(c.getNome()));
+						document.add(new Paragraph("Candidatos:"));
+						for (Candidato ca : candidatos) {
+							if (ca.getCargo().getId() == c.getId()) {
+								document.add(new Paragraph(ca.getNome()));
+							}
+						}
+					}
+
+				}
+
+				document.add(
+						new Paragraph("-----------------------------------------------------------------------------"));
+			}
+		} catch (Exception e) {
 		}
-		
+
 	}
 }
