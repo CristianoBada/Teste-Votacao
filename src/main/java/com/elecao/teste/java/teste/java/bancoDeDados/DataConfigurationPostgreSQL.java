6527653c4aa27286937cede762d6f3a0f03b15fa
Cristiano Badalotti <cristiano.badalotti@gmail.com>
2020-04-08 23:34:56 -0300

    010 - Alteração da dependencia do heroku com postgresql

--- src/main/java/com/elecao/teste/java/teste/java/bancoDeDados/DataConfigurationPostgreSQL.java
+++ src/main/java/com/elecao/teste/java/teste/java/bancoDeDados/DataConfigurationPostgreSQL.java
@@ -13,20 +13,21 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Profile("prod")
 public class DataConfigurationPostgreSQL {
-	
+
 	@Bean
-    public BasicDataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+	public BasicDataSource dataSource() throws URISyntaxException {
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath()
+				+ "?sslmode=require";
 
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
+		BasicDataSource basicDataSource = new BasicDataSource();
+		basicDataSource.setUrl(dbUrl);
+		basicDataSource.setUsername(username);
+		basicDataSource.setPassword(password);
 
-        return basicDataSource;
-    }
+		return basicDataSource;
+	}
 }
