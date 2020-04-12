7b7a953d231a3b2135ee5c0247e68598521e8203
Cristiano Badalotti <cristiano.badalotti@gmail.com>
2020-04-12 20:12:37 -0300

    030 - teste com sequence individual por tabela

--- src/main/java/com/elecao/teste/java/teste/java/Entidades/Eleicao.java
+++ src/main/java/com/elecao/teste/java/teste/java/Entidades/Eleicao.java
@@ -1,16 +1,24 @@
 package com.elecao.teste.java.teste.java.Entidades;
 
+import java.io.Serializable;
 import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 @Entity
-public class Eleicao extends AbstractEntity {
+public class Eleicao implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "seq_eleicao")
+	private Long id;
 
 	// Nome da eleição
 	@NotEmpty
@@ -28,6 +36,14 @@ public class Eleicao extends AbstractEntity {
 
 	@OneToMany
 	private Set<Cargo> cargos;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	@OneToMany
 	private Set<Eleitor> eleitores;
@@ -75,6 +91,31 @@ public class Eleicao extends AbstractEntity {
 
 	public void setFim(String fim) {
 		this.fim = fim;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Eleicao other = (Eleicao) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 }
