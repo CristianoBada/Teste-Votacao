6d21459b041c2c37e6b980f9e2af2484547336f9
Cristiano Badalotti <cristiano.badalotti@gmail.com>
2020-04-12 16:42:20 -0300

    026 - correção para não se candidatar em uma eleição que esta em andamento ou finalizada

--- src/main/java/com/elecao/teste/java/teste/java/Controles/CandidatoControle.java
+++ src/main/java/com/elecao/teste/java/teste/java/Controles/CandidatoControle.java
@@ -1,6 +1,7 @@
 package com.elecao.teste.java.teste.java.Controles;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.validation.Valid;
 
@@ -15,9 +16,12 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.elecao.teste.java.teste.java.Entidades.Candidato;
+import com.elecao.teste.java.teste.java.Entidades.Cargo;
+import com.elecao.teste.java.teste.java.Entidades.Eleicao;
 import com.elecao.teste.java.teste.java.Repository.CandidatoRepository;
 import com.elecao.teste.java.teste.java.Repository.CargoRepository;
-
+import com.elecao.teste.java.teste.java.Repository.EleicaoRepository;
+import com.elecao.teste.java.teste.java.Util.ConvercoesDeDatas;
 
 @Controller
 public class CandidatoControle {
@@ -28,22 +32,49 @@ public class CandidatoControle {
 	@Autowired
 	private CandidatoRepository candidatoDAO;
 
+	@Autowired
+	private EleicaoRepository eleicaoDAO;
+
+	private Eleicao eleicao;
+
+	private List<Cargo> cargos;
+
 	@RequestMapping("/cadastroCandidato")
+	public String CadastrarCandidatoProximo(Model model) {
+		model.addAttribute("listaEleicao", eleicaoDAO.findByInicioAfter(ConvercoesDeDatas.dataAtualUS()));
+		return "candidato/CadastroCandidatoEleicao";
+	}
+
+	@RequestMapping("/cadastroCandidatoProximo")
 	public String CadastrarCandidato(Model model) {
+		model.addAttribute("nomeEleicao", eleicao.getNome());
 		model.addAttribute("candidato", new Candidato());
-		model.addAttribute("listaCargo", cargoDao.findAll());
+		model.addAttribute("listaCargo", cargos);
 		return "candidato/CadastroCandidato";
 	}
 
+	@PostMapping(value = "/cadastroCandidatoEleicao/proximo")
+	public String EleicaoProxima(@Valid Eleicao eleicao, BindingResult result, RedirectAttributes attributes) {
+		cargos = cargoDao.findByEleicao_id(eleicao.getId());
+		if (cargos.size() > 0) {
+			this.eleicao = eleicao;
+			return "redirect:/cadastroCandidatoProximo";
+		} else {
+			attributes.addFlashAttribute("mensagemAviso", "Não existe cargos ne eleição: " + eleicao.getNome());
+			 return "redirect:/cadastroCandidato";
+		}
+	}
+
 	@PostMapping(value = "/cadastroCandidato/novo")
-	public String novaEleicao(@Valid Candidato candidato, @RequestParam("imageFile") MultipartFile imageFile, BindingResult result, RedirectAttributes attributes) {
+	public String novaEleicao(@Valid Candidato candidato, @RequestParam("imageFile") MultipartFile imageFile,
+			BindingResult result, RedirectAttributes attributes) {
 		if (result.hasErrors()) {
 			attributes.addFlashAttribute("mensagemAviso", "Verifique os campos!");
 		} else {
 			candidato.setVotos(0);
 			try {
 				if (imageFile.getBytes().length > 0) {
-				candidato.setImagem(imageFile.getBytes());
+					candidato.setImagem(imageFile.getBytes());
 				} else {
 					attributes.addFlashAttribute("mensagemAviso", "Selecione uma imagem!");
 					return "redirect:/cadastroCandidato";
@@ -58,4 +89,5 @@ public class CandidatoControle {
 
 		return "redirect:/cadastroCandidato";
 	}
+
 }
